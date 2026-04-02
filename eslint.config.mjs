0c01a55ab77f9090a4c00c4c4d0e48f7b79e4dfc
Hoang Dinh <hoang.dv@outlook.com>
2026-04-02 12:28:08 +0900

    chore: fix npm audit issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,22 @@
-import base from 'eslint-config-rchl-base';
-import typescript from 'eslint-config-rchl-typescript';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    ...base,
-    ...typescript,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+            '@stylistic/semi': ['error', 'always'],
+            '@stylistic/comma-dangle': ['error', 'always-multiline'],
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        },
+    },
     {
         ignores: ['dist/', 'public/vendor/'],
     },
